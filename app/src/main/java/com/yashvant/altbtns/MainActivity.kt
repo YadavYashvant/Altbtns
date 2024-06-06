00eec129e9398ebdd40ba595de16250f736d54d7
YadavYashvant <yashvantyadav855@gmail.com>
2024-06-06 19:47:54 +0530

    made implementations

--- app/src/main/java/com/yashvant/altbtns/MainActivity.kt
+++ app/src/main/java/com/yashvant/altbtns/MainActivity.kt
@@ -3,68 +3,39 @@ package com.yashvant.altbtns
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.yashvant.altbtns.R
-import com.yashvant.altbtns.utils.COMMON_BORDER_WIDTH
-import com.yashvant.altbtns.utils.COMMON_CORNER_RADIUS
-import com.yashvant.altbtns.utils.COMMON_HEIGHT
-import com.yashvant.altbtns.utils.COMMON_WIDTH
-import com.yashvant.altbtns.utils.FONT_SIZE
-import com.yashvant.altbtns.utils.TOP_BUTTON_HEIGHT
-import com.yashvant.altbtns.utils.TOP_BUTTON_RADIUS
-import com.yashvant.altbtns.utils.FORTY
-import com.yashvant.altbtns.utils.ONE_FLOAT
-import com.yashvant.altbtns.utils.TWELVE
-import com.yashvant.altbtns.utils.TWO
-import com.yashvant.altbtns.utils.ZERO
-import com.yashvant.altbtns.utils.ten
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            var altBtnState by remember { mutableStateOf(AltBtnState.IDLE) }
+            val scope = rememberCoroutineScope()
+
+            AltBtn(
+                type = AltBtnType.CLOCK,
+                width = 200.dp,
+                height = 70.dp,
+                onClick = {
+                    altBtnState = AltBtnState.LOADING
+                    scope.launch {
+                        delay(2000)
+                    }
+                    altBtnState = AltBtnState.SUCCESS
+                },
+                assetColor = Color.Cyan,
+                buttonState = altBtnState
+            )
 
         }
     }
