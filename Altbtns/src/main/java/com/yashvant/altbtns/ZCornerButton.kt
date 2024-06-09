0f9773aabd187f7cd47a14f86237599a1c518213
YadavYashvant <yashvantyadav855@gmail.com>
2024-06-09 23:12:15 +0530

    stacked button almost done

--- Altbtns/src/main/java/com/yashvant/altbtns/ZCornerButton.kt
+++ Altbtns/src/main/java/com/yashvant/altbtns/ZCornerButton.kt
@@ -1,2 +1,25 @@
 package com.yashvant.altbtns
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ZCornerButton(){
+
+    Button(
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(0),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green),
+        border = BorderStroke(1.dp, Color.Yellow)
+    ) {
+        Text(text = "ZCornerButton")
+    }
+
+}
+
--- app/src/main/java/com/yashvant/altbtns/MainActivity.kt
+++ app/src/main/java/com/yashvant/altbtns/MainActivity.kt
@@ -10,7 +10,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
@@ -98,5 +103,16 @@ class MainActivity : ComponentActivity() {
 
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAltBtn() {
+    Button(
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(0)
+    ) {
+        Text(text = "ZCornerButton")
     }
 }
